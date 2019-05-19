8371fb00526e5d82f611b2c08f8f92f00d622585
gmw-zjw <mingwei2222@126.com>
2019-05-19 09:07:19 +0800

    为cart_使用数据模型

--- lib/provide/cart.dart
+++ lib/provide/cart.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
+import '../model/cartInfo.dart';
 
 class CartProvide with ChangeNotifier {
   String cartString = '[]';
+  List<CartInfoModel> cartList = [];
 
 // 保存商品数据
   save(goodsId, goodsName, count, price, images) async {
@@ -19,6 +21,8 @@ class CartProvide with ChangeNotifier {
     tempList.forEach((item) {
       if (item['goodsId'] == goodsId) {
         tempList[ival]['count'] = item['count']+1;
+        // 数据模型
+        cartList[ival].count+1;
         isHave = true;
       }
 
@@ -27,18 +31,21 @@ class CartProvide with ChangeNotifier {
 
 // 如果没有值， 则添加
     if (!isHave) {
-      tempList.add({
+      Map<String, dynamic> newGoods = {
         'goodsId': goodsId,
         'goodsName': goodsName,
         'count': count,
         'price': price,
         'images': images
-      });
+      };
+      tempList.add(newGoods);
+      cartList.add(CartInfoModel.fromJson(newGoods)); // 模型的fromdata
     }
 
     cartString = json.encode(tempList).toString();
     print(cartString);
     prefs.setString('cartInfo', cartString);// 持久化数
+    print('数据模型>>>${cartList}');
 
     notifyListeners(); // 发射数据
   } 
@@ -47,6 +54,7 @@ class CartProvide with ChangeNotifier {
   remove() async{
      SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.remove('cartInfo');
+    cartList = []; // 清空模型数据
     print('清空完成........');
 
     notifyListeners();
