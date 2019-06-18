cf1581c49cf3dc66efa1e42e3c153b448a82d69a
gmw-zjw <mingwei2222@126.com>
2019-06-18 15:55:53 +0800

    修复bug

--- lib/config/service_url.dart
+++ lib/config/service_url.dart
@@ -1,4 +1,5 @@
 const serviceUrl = 'http://v.jspang.com:8088/baixing/';
+// const serviceUrl = 'http://localhost:3001/';
 const servicePath  = {
   'homePageContent': serviceUrl + 'wxmini/homePageContent', //商店页面详情
   'homePageBelowConten': serviceUrl + 'wxmini/homePageBelowConten', // 商城首页火爆专区
@@ -6,3 +7,11 @@ const servicePath  = {
   'getMallGoods': serviceUrl + 'wxmini/getMallGoods', // 商品分类商品列表
   'getGoodDetailById': serviceUrl + 'wxmini/getGoodDetailById', // 商品描述详细页
 };
+
+// const servicePath = {
+//   'homePageContent': serviceUrl + 'home/getHomePages',
+//   'homePageBelowConten': serviceUrl + 'home/getHotsAll',
+//   'getCategory': serviceUrl + 'category/getCategory',
+//   'getMallGoods': serviceUrl + 'category/getMallGoods',
+//   'getGoodDetailById': serviceUrl + 'category/getGoodsDetailById'
+// };
